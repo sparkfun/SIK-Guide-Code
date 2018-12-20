1eb93602e90eba8a28f6b730b1149eec1619c270
Ho Yun "Bobby <ho.chan@colorado.edu>
2018-12-20 14:54:33 -0700

    Update SIK_Circuit_3C-MotionAlarm.ino
    Update comment to reflect pin definitions

--- SIK_Circuit_3C-MotionAlarm/SIK_Circuit_3C-MotionAlarm.ino
+++ SIK_Circuit_3C-MotionAlarm/SIK_Circuit_3C-MotionAlarm.ino
@@ -13,8 +13,8 @@ Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
 
 #include <Servo.h>                //include the servo library
 
-const int trigPin = 11;           //connects to the echo pin on the distance sensor       
-const int echoPin = 12;           //connects to the trigger pin on the distance sensor     
+const int trigPin = 11;           //connects to the trigger pin on the distance sensor       
+const int echoPin = 12;           //connects to the echo pin on the distance sensor     
 
 const int redPin = 3;             //pin to control the red LED inside the RGB LED
 const int greenPin = 5;           //pin to control the green LED inside the RGB LED
